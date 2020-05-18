6d369507d41311de25951d855d6a9cfbc374c2ca
andradecode <andradecode@outlook.com>
2020-05-18 16:13:35 -0300

    Add ex008

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # exercicios
  Alguns exercícios respondidos para treino
+
+
+font: https://github.com/gustavoguanabara/javascript/tree/master/exercicios
--- testes.js
+++ testes.js
@@ -1,10 +1,8 @@
-var n1 = ('2,9000').replace(",",".")
+var n1 = ('100').replace(",",".")
+var n2 = n1
 
-var n2 = Number(n1).toFixed(2)
-
-var n3 = ('3,2032').replace(",",".")
-
-var n4 = Number(n3).toFixed(2)
+var n3 = n2 * 0.1
 
 console.log(n2)
-console.log(n4)
+console.log(n3)
+// Number(n1).toLocaleString('pt-br', {style:'currency', currency: 'BRL'})
